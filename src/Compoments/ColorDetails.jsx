337894390e4136f268f278b3f3f7b5752b12ef39
Deependra <deependra186037@gamil.com>
2024-03-04 18:03:52 +0530

    Update json file

--- src/Compoments/ColorDetails.jsx
+++ src/Compoments/ColorDetails.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { ColorsData } from "./ColorsData";
+import Data from "./Data.json"
 import ColorCard from "./ColorCard";
 function ColorDetails() {
   return (
     <div className="flex flex-wrap justify-center items-center gap-10 py-16 px-16">
-      {ColorsData.map((e,i) => {
+      {Data.map((e,i) => {
         return <ColorCard bg = {e.color} name = {e.name} key={i} />;
       })}
     </div>
